7d718e9ed23d731f6a6954a37944db82fd6cd6ca
Daniele Volpato <volpato.daniele@iisgalvanimi.edu.it>
2020-02-10 09:50:09 +0000

    6.1

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -1,26 +1,34 @@
 var express = require('express');
 var router = express.Router();
-const sql = require('mssql');
+const sql = require('mssql')
 
 const config = {
-  user: '4DD_23',  //Vostro user name
-  password: 'xxx123##', //Vostra password
-  server: "213.140.22.237",  //Stringa di connessione
-  database: '4DD_23', //(Nome del DB)
+  user: '4dd_23', 
+  password: 'xxx123##',
+  server: "213.140.22.237\\sqlexpress",
+  database: '4dd_23', 
 }
 
-/* GET users listing. */
-router.get('/attori', function(req, res, next) {
+router.get('/', function(req, res, next) {
   sql.connect(config, err => {
-    if(err) console.log(err);  // ... error check
+    if(err) console.log(err); 
     
-    // Query
-    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from dbo.AA_ATTORE', (err, result) => {
-        if (err) console.log(err); // ... error checks
-        res.send(result);  //Invio il risultato
+    let sqlRequest = new sql.Request();
+    sqlRequest.query('select * from Persona', (err, result) => {
+        if (err) console.log(err); 
+        res.send(result);  
     });
   });
 });
+router.get('/search/:name/', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);
+    let sqlRequest = new sql.Request();
+    sqlRequest.query(`select * from Persona where Nome = '${req.params.name}'`, (err, result) => {
+        if (err) console.log(err);
 
+        res.send(result);
+    });
+  });
+});
 module.exports = router;
